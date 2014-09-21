33aa9f0c743f3c4f00e05e4a63e39fc5be4e9121
ganaraj <ganarajpr@gmail.com>
2014-09-21 13:38:24 +0100

    Upgrade ext-accessor.

--- common/models/project.js
+++ common/models/project.js
@@ -1,9 +1,7 @@
 var acc = require("ext-accessor");
 module.exports = function(Project){
     Project.functionexternals = function(fnSource, cb) {
-        console.log(fnSource);
         fnSource = fnSource.replace(/(\r\n|\n|\r)/gm,"");
-        console.log(fnSource);
         var extAcc = acc.getAccessors(fnSource);
         cb(null,extAcc);
     };
